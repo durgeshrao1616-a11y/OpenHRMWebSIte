9d78291f8e01552cd67d8a5c25dece7899bbae5b
durgeshrao1616-a11y <durgeshrao1616@gmail.com>
2026-01-15 14:35:00 +0530

    Added dotenv config and Playwright test updates

--- playwright.config.js
+++ playwright.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'allure-playwright',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
@@ -65,7 +65,7 @@ export default defineConfig({
     screenshot: 'on',
     video: 'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
   },
 
   /* Configure projects for major browsers */
